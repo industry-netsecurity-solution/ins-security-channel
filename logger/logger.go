25a1afe2dc596f830848d2c4dbbe4279bf20fa7c
kim-sj <sfline@ymtech.co.kr>
2021-08-02 10:17:28 +0900

    add logger

---
+++ logger/logger.go
@@ -0,0 +1,54 @@
+package logger
+
+import (
+	"os"
+	"log"
+)
+
+type LogWriter interface {
+	Critiacal(args ... interface{})
+	Error(args ... interface{})
+	Warning(args ... interface{})
+	Debug(args ... interface{})
+	Info(args ... interface{})
+}
+
+type Logger struct {
+	CRITICAL *log.Logger
+	ERROR *log.Logger
+	WARN *log.Logger
+	DEBUG *log.Logger
+	INFO *log.Logger
+}
+
+func New() LogWriter {
+	l := new (Logger)
+	l.ERROR = log.New(os.Stdout, "[ERROR] ", 0)
+	l.CRITICAL = log.New(os.Stdout, "[CRIT] ", 0)
+	l.WARN = log.New(os.Stdout, "[WARN]  ", 0)
+	l.DEBUG = log.New(os.Stdout, "[DEBUG] ", 0)
+	l.INFO = log.New(os.Stdout, "[INFO] ", 0)
+
+	return l
+}
+
+func (v Logger) Critiacal(args ... interface{}) {
+	v.CRITICAL.Println(args)
+}
+
+func (v Logger) Error(args ... interface{}) {
+	v.ERROR.Println(args)
+}
+
+func (v Logger) Warning(args ... interface{}) {
+	v.WARN.Println(args)
+}
+
+func (v Logger) Debug(args ... interface{}) {
+	v.DEBUG.Println(args)
+}
+
+func (v Logger) Info(args ... interface{}) {
+	v.INFO.Println(args)
+}
+
