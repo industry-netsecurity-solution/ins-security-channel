2c71854e20276acbae426e5aefc2dee1202061cf
kim-sj <sfline@ymtech.co.kr>
2021-08-03 14:37:55 +0900

    add std logger api

--- logger/logger.go
+++ logger/logger.go
@@ -48,7 +48,7 @@ func New(out io.Writer, flag int) *Logger {
 	return l
 }
 
-func (v Logger) LogWriter() LogWriter {
+func (v Logger) Writer() LogWriter {
 	return LogWriter(v)
 }
 
@@ -164,4 +164,120 @@ func (v Logger) Printf(format string, args ... interface{}) {
 
 func (v Logger) Println(args ... interface{}) {
 	v.NONE.Println(args...)
+}
+
+
+//-----------------------------------------------------
+
+var std = New(os.Stderr, log.LstdFlags)
+
+
+func Writer() LogWriter {
+	return std.Writer()
+}
+
+func SetOutput(w io.Writer) {
+	std.SetOutput(w)
+}
+
+// Fatalln is equivalent to l.Println() followed by a call to os.Exit(1).
+func Fatal(args ...interface{}) {
+	std.Fatal(args...)
+}
+
+// Fatalf is equivalent to l.Printf() followed by a call to os.Exit(1).
+func Fatalf(format string, args ...interface{}) {
+	std.Fatalf(format, args...)
+}
+
+// Fatalln is equivalent to l.Println() followed by a call to os.Exit(1).
+func Fatalln(args ...interface{}) {
+	std.Fatalln(args...)
+}
+
+// Panic is equivalent to l.Print() followed by a call to panic().
+func Panic(args ...interface{}) {
+	std.Panic(args...)
+}
+
+// Panicf is equivalent to l.Printf() followed by a call to panic().
+func Panicf(format string, args ...interface{}) {
+	std.Panicf(format, args...)
+}
+
+// Panicln is equivalent to l.Println() followed by a call to panic().
+func Panicln(args ...interface{}) {
+	std.Panicln(args...)
+}
+
+// Flags returns the output flags for the logger.
+// The flag bits are Ldate, Ltime, and so on.
+func Flags() int {
+	return std.Flags()
+}
+
+// SetFlags sets the output flags for the logger.
+// The flag bits are Ldate, Ltime, and so on.
+func SetFlags(flag int) {
+	std.SetFlags(flag)
+}
+
+func Error(args ... interface{}) {
+	std.Error(args...)
+}
+
+func Errorf(format string, args ... interface{}) {
+	std.Errorf(format, args...)
+}
+
+func Errorln(args ... interface{}) {
+	std.Errorln(args...)
+}
+
+func Warning(args ... interface{}) {
+	std.Warning(args...)
+}
+
+func Warningf(format string, args ... interface{}) {
+	std.Warningf(format, args...)
+}
+
+func Warningln(args ... interface{}) {
+	std.Warningln(args...)
+}
+
+func Debug(args ... interface{}) {
+	std.Debug(args...)
+}
+
+func Debugf(format string, args ... interface{}) {
+	std.Debugf(format, args...)
+}
+
+func Debugln(args ... interface{}) {
+	std.Debugln(args...)
+}
+
+func Info(args ... interface{}) {
+	std.Info(args...)
+}
+
+func Infof(format string, args ... interface{}) {
+	std.Infof(format, args...)
+}
+
+func Infoln(args ... interface{}) {
+	std.Infoln(args...)
+}
+
+func Print(args ... interface{}) {
+	std.Print(args...)
+}
+
+func Printf(format string, args ... interface{}) {
+	std.Printf(format, args...)
+}
+
+func Println(args ... interface{}) {
+	std.Println(args...)
 }
