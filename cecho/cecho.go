b5a41f1c43f6cd441197dcf485776e418e06d733
kim-sj <sfline@ymtech.co.kr>
2022-08-03 20:34:06 +0900

    echo 테스트용 ciphersuite 추가

--- cecho/cecho.go
+++ cecho/cecho.go
@@ -70,6 +70,10 @@ func Start(config *ins.ServiceConfigurations, ud interface{}, callback func(*ech
 				CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 				CipherSuites: []uint16{
 					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_CHACHA20_POLY1305_SHA256,
+					tls.TLS_AES_256_GCM_SHA384,
 					tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 					tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
 					tls.TLS_RSA_WITH_AES_256_CBC_SHA,
