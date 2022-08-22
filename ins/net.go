fed8496db404c221432851131df61be34d5bb2f0
kim-sj <sfline@ymtech.co.kr>
2022-08-22 15:15:21 +0900

    TLS 클라이언트 cipersuite 적용: TLS1.2, TLS_xxx 알고리즘

--- ins/net.go
+++ ins/net.go
@@ -53,6 +53,20 @@ func NewTLSConfig(cacertFile *string, certFile *string, keyFile *string) *tls.Co
 
 	// Create net.Config with desired net properties
 	return &tls.Config{
+		MinVersion:       tls.VersionTLS12,
+		CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		CipherSuites: []uint16{
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			//tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			//tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			//tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+
 		// RootCAs = certs used to verify server cert.
 		RootCAs: certpool,
 		// ClientAuth = whether to request cert from server.
